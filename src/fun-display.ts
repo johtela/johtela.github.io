a4be227ee5a5d0f9d2eb60e59060800501e6d86f
johtela <tommi.johtela@dnainternet.net>
2018-12-10 17:05:01 +0200

    Added skeleton for new display.

---
+++ src/fun-display.ts
@@ -0,0 +1,15 @@
+import  * as THREE from "three";
+import { canvasHeight, msInSec, Display } from './display';
+
+export class FunDisplay extends Display {
+    private renderer: THREE.Renderer;
+    private scene: THREE.Scene;
+    private camera: THREE.Camera;
+
+    constructor(content: HTMLElement) {
+        super(content);
+        this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, this.canvas.width / this.canvas.height, 0.1, 1000);
+    }
+}
