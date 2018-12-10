ad566ca7b71b41d708a31d97fa2b817f43db59f1
johtela <tommi.johtela@dnainternet.net>
2018-12-10 23:42:44 +0200

    Intermediate commit.

--- src/fun-display.ts
+++ src/fun-display.ts
@@ -11,5 +11,8 @@ export class FunDisplay extends Display {
         this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, this.canvas.width / this.canvas.height, 0.1, 1000);
+        this.camera.position.set(0, 0, 100);
+        this.camera.lookAt(0, 0, 0);
+        
     }
 }
